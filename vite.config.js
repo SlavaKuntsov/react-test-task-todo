556343d75de3c74727b43eb3b8a2aea219e4b292
Slava <kuncovs19@gmail.com>
2023-07-27 09:17:30 +0300

    fix v4

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-	exclude: ["@chakra-ui/system"]
-  }
+	plugins: [react()],
+	optimizeDeps: {
+		exclude: [
+			'@chakra-ui/system',
+			'@chakra-ui/icons',
+			'axios',
+			'@types/react',
+			'@types/react-dom'
+		]
+	}
 })

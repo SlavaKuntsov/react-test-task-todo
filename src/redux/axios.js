ff97d81ef349f7e4bb1902921de70c21ab393e70
Slava <kuncovs19@gmail.com>
2023-07-27 08:17:55 +0300

    fix

--- src/redux/axios.js
+++ src/redux/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:9998'
+axios.defaults.baseURL = 'https://test-task-backend-host.onrender.com'
+// axios.defaults.baseURL = 'http://localhost:9998'
 axios.defaults.headers.common['token'] = localStorage.getItem('token')
 // axios.defaults.baseURL = 'http://localhost:9998'
 
